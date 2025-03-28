ddbd972f71a38b09dc273b35e5be59a1b19dd96d
jymontero <jmontero@ingenioso.co>
2025-03-28 20:23:56 +0000

    [UPD] Se actualizo mensajes de confirmacion  de codigo barrasd

--- wizards/product_template_barcode.py
+++ wizards/product_template_barcode.py
@@ -19,28 +19,45 @@ class DynamicProductBarcode(models.TransientModel):
         print('Confirm barcode generator')
         self.ensure_one()
         product_ids = self.env['product.template'].browse(self._context.get('active_ids'))
+        existing_code_products = []
+
 
         for prod in product_ids:
             product_id = prod.id
             product_product_ids = self.env['product.product'].search([('product_tmpl_id','=',product_id)])
             print('product_product_id:',product_product_ids)
             for prod_prod_id in product_product_ids:
-                print('prod_prod_id:',prod_prod_id)
                 product_product_id = prod_prod_id.id
                 if prod_prod_id.barcode == False or prod_prod_id == "":
                     eanbarcode = barcode.generate_ean(self, str(product_product_id))
                     self.env.cr.execute(
                         "update product_product set barcode='" + str(eanbarcode) + "' where id='" + str(product_product_id) + "'")
                 else:
-                    print("Producto con codigo de barras YA GENERADO")
-                    return {
-                        'type': 'ir.actions.client',
-                        'tag': 'display_notification',
-                        'params': {
-                            'title': 'Aviso',
-                            'message': f'El producto "{prod_prod_id.name}" ya tiene un código de barras asignado.',
-                            'sticky': False,  # Si True, el mensaje no desaparece automáticamente
-                            'type': 'warning',  # Puede ser success, warning, danger, info
-                        }
-                    }
+                    existing_code_products.append(prod_prod_id.name)
 
+        messages = []                                    
+        if existing_code_products:
+            messages.append({
+                'type': 'warning',
+                'title': _('Advertencia'),
+                'message': _('Algunos productos ya tienen código de barras: %s') % ', '.join(existing_code_products)
+            })
+            
+        action = {'type': 'ir.actions.act_window_close'}
+            
+        if messages:    
+            first_msg = messages[0]
+            notification = {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'title': first_msg['title'],
+                        'message': first_msg['message'],
+                        'sticky': False,
+                        'type': first_msg['type'],
+                        'next': action,
+                    }
+                }
+            return notification
+        return action                
+        
